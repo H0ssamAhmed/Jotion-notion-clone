0ecc750f97e4768a7d48fd0f6afd25583ad09f5f
Hossam Ahmed <hossamahmed8862@gmail.com>
2024-01-13 18:29:20 +0200

    Deploy

--- app/(marketing)/_components/Heroes.js
+++ app/(marketing)/_components/Heroes.js
@@ -24,14 +24,14 @@ const Heroes = () => {
                 </div>
                 <div className='relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px] hidden md:block'>
                     <Image
-                        src="/Reading.png"
+                        src="/reading.png"
                         fill
                         className='object-contain dark:hidden'
                         alt="Reading"
                         size={45}
                     />
                     <Image
-                        src="/Reading-dark.png"
+                        src="/reading-dark.png"
                         fill
                         className='object-contain  hidden dark:block'
                         alt="Reading"
